7c7225b9d29585d4ffde2d6f2659fb07ed1bd6bd
Leulseged <leulbekele191@gmail.com>
2023-01-05 10:21:42 +0300

    check if boxes can be unlocked

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
-"""this module checks if the boxes can be unlocked
+"""This module checks if the boxes can be unlocked
 from the values of other boxes
 """
 
 
 def canUnlockAll(boxes):
-    """this method checkes if all boxes are opened"""
+    """This method checkes if all boxes are opened
+       Also opens locked boxes if possible
+    """
     size = len(boxes)
     checker = []
-    # initialize the array indexes to opened false and visited false
+    # Initialize the array indexes to opened false and visited false
     for i in range(size):
         checker.append({"opened": False, "visited": False})
-    # first array shall be opened true
+    # First array shall be opened true
     checker[0]["opened"] = True
-    # done checkes if the any boxe is opened and visited
+    # Done checkes if the any boxe is opened and visited
     done = False
-    # visite the boxes when the boxes are opened but not visited
+    # Visite the boxes when the boxes are opened but not visited
     while(done is False):
         done = True
-        # try opening boxes based on the values inside the opened boxes
+        # Try opening boxes based on the values inside the opened boxes
         for i in range(size):
             if (checker[i]['opened'] is True and
                     checker[i]['visited'] is False):
                 for j in range(len(boxes[i])):
                     checker[boxes[i][j]]['opened'] = True
                 checker[i]['visited'] = True
-        # check if ther is any remaining box that is opened and not visited
+        # Check if ther is any remaining box that is opened and not visited
         for i in range(size):
             if (checker[i]['opened'] is True and
                     checker[i]['visited'] is False):
                 done = False
-    # check if all the boxes have been opened
+    # Check if all the boxes have been opened
     for i in range(size):
         if (checker[i]['opened'] is False):
             return False
