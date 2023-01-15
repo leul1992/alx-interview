bb16354b9d55e67b3ed272e6dd435e346ffe1ea6
Leulseged <leulbekele191@gmail.com>
2023-01-15 09:06:31 +0300

    find min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,19 @@
-""" Module finds the minimum number
-of operation to reach a result"""
+"""
+Module finds the minimum number
+of operation to reach a result
+"""
 
 
 def minOperations(n):
-    """ Method for minimum operations
-    :param int n: The number to reach"""
+    """
+    Method for minimum operations
+    
+    Parameters:
+    n (int): The number to reach
+    
+    Returns:
+    int: either 0 or the number of steps
+    """
     final = ''
     paste = 'H'
     counter = 0
