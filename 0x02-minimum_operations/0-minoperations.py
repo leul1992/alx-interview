269805310191e7809b4d09aae350b5cd1f9c38ed
Leulseged <leulbekele191@gmail.com>
2023-01-15 09:31:39 +0300

    find min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,34 @@
-"""
-Module finds the minimum number
-of operation to reach a result
-"""
+"""Module finds the minimum number of operation"""
 
 
 def minOperations(n):
     """
     Method for minimum operations
-    
     Parameters:
-    n (int): The number to reach
-    
+        n (int): The number to reach
     Returns:
-    int: either 0 or the number of steps
+        Integer: either 0 or the number of steps
     """
-    final = ''
-    paste = 'H'
-    counter = 0
+    final = ''  # the final n number of chars reached
+    paste = 'H'  # the paste character
+    counter = 0  # the number of steps
     while len(final) < n:
+        # if nothing or only one is copied
         if len(final) == 0 or len(final) == 1:
+            # copy the copied char on paste variable
             final += paste
         else:
+            # if the length of final is multiple of n
             if n % len(final) == 0:
+                # copy all to the paste
                 paste = final
+                # increament b/c copy all step is used
                 counter += 1
+            # paste to the final both if and else
             final += paste
+        # add count as one step of paste is passed
         counter += 1
+    # after looping if length of final is not equal to n
     if len(final) != n:
         return 0
     return counter
