f1bd667e5b830bfb15d0989a3ea3f3bdf4d271ad
Leulseged <leulbekele191@gmail.com>
2023-01-18 15:48:23 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,8 +2,9 @@
 """ A script that reads stdin line by line and
 computes metrics
 """
-import sys
 
+
+import sys
 
 counter = 0
 totalSize = 0
@@ -12,6 +13,7 @@ final = {
     '401': 0, '403': 0, '404': 0,
     '405': 0, '500': 0
  }
+
 try:
     for line in sys.stdin:
         line = line.split(" ")
@@ -23,17 +25,18 @@ try:
                 final[line[-2]] += 1
             totalSize += int(line[-1])
             counter += 1
+
             if counter % 10 == 0:
                 print(f'File size: {totalSize}')
                 for fi in final:
                     if final[fi] != 0:
                         print(f"{fi}: {final[fi]}")
-                final = final.fromkeys(final, 0)
+
 except Exception as err:
     pass
+
 finally:
     print(f'File size: {totalSize}')
     for fi in final:
         if final[fi] != 0:
             print(f"{fi}: {final[fi]}")
-    final = final.fromkeys(final, 0)
