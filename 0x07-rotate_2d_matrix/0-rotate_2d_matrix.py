357057388e0069dadb7e29e914c02ad463eb6d36
Leulseged <leulbekele191@gmail.com>
2023-02-16 13:52:26 +0300

    rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,13 +7,7 @@ def rotate_2d_matrix(matrix):
     a method to apply the rotation
     """
     length = len(matrix)
-    for i in range(length):
-        if length % 2 == 0 and i > length / 2:
-            break
-        elif length % 2 != 0 and i > int(length) / 2:
-            break
-        matrix[i], matrix[length - (i + 1)] =\
-            matrix[length - (i + 1)], matrix[i]
+    matrix.reverse()
     for i in range(length):
         for j in range(i + 1, len(matrix[i])):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
