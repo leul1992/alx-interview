54db8b7cdee253ccae8711940962c41955ae4abe
Leulseged <leulbekele191@gmail.com>
2023-02-24 01:15:13 +0300

    makechange

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module to figure out the minimum num of coins that match total """
+
+
+def makeChange(coins, total):
+
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total == 0:
+            break
+        if coin <= total:
+            count += total // coin
+            total %= coin
+    if total == 0:
+        return count
+    return -1
